21e36ef6c7059a10fc4bc710a718fea2973c5273
Yuming-lab <yuming80717@gmail.com>
2024-05-26 13:12:07 +0900

    changed the content of main()

--- akito.py
+++ akito.py
@@ -2,9 +2,10 @@
 
 def main():
     print("hello, my name is AKito!")
+    print("Nice to meet you Akito! How can I help you?")
 
 
-if __init__==__main():
-    main()))
+if "__init__"=="__main__":
+    main()
 
 
